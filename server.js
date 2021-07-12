3afa177a27a5bd8d62a39653f1c33428f47a5238
Emily Pozzi <epozzi5@yahoo.com>
2021-07-11 22:10:04 -0500

    linked html files and created server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const mySQL = require("mysql");
 const express = require("express");
+
+const fs = require("fs");
+const path = require("path");
+
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(dirname, "./Develop/public/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
