8402f56ba8684222564b1caadf3bbe9c6c0e46e5
Emily Pozzi <epozzi5@yahoo.com>
2021-07-11 19:59:04 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const mySQL = require("mysql");
+const express = require("express");
+var app = express();
+var PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
