f4f9509d8c7765f5366d961ca92cd5bb368ddaae
Emily Pozzi <epozzi5@yahoo.com>
2021-07-11 22:47:53 -0500

    linked index to html

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(dirname, "./Develop/public/index.html"));
 });
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+})
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
